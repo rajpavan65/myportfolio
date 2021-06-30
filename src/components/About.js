6e3f0615abc963aaadf3ee1094d23841ba90652e
Pavan <rajpavan6593@gmail.com>
2021-06-30 17:05:45 +0530

    about modified

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ function About() {
                     <img src='./Images/dp.jpeg' alt='dp' />
                     <h1>Hi, I'm Pavan.</h1>
 
-                    <h2>A web designer and developer from Southampton in the India. I create bespoke websites to help people go further online.</h2>
+                    <h2>A web designer and developer from Damoh, India. I create bespoke websites to help people go further online.</h2>
                     <br></br>
 
                     <p style={{ textAlign: 'justify', marginBottom: '20px' }}>Since beginning my journey as a freelance developer nearly 2 years ago, I've done remote work for agencies, startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.</p>
